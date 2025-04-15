9bd19faf597527f12743a0ddc35f580c05b32bac
Matthew McKenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2025-04-15 16:35:21 -0500

    dual button fix

--- includes/class-sxs-candidate-comparison.php
+++ includes/class-sxs-candidate-comparison.php
@@ -167,8 +167,8 @@ class SXS_Candidate_Comparison {
         ob_start();
         
         // Get button settings and URLs
-        $enable_position_brief = get_post_meta($atts['set'], '_sxs_enable_position_brief', true);
-        $enable_scorecard = get_post_meta($atts['set'], '_sxs_enable_scorecard', true);
+        $enable_position_brief = get_post_meta($atts['set'], '_sxs_position_brief_enabled', true);
+        $enable_scorecard = get_post_meta($atts['set'], '_sxs_scorecard_enabled', true);
         $position_brief_url = get_post_meta($atts['set'], '_sxs_position_brief_url', true);
         $scorecard_url = get_post_meta($atts['set'], '_sxs_scorecard_url', true);
         
